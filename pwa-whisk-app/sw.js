87b10f278798143674e0e062d6b36680e909a654
apatil88 <amrut.patil88@hotmail.com>
2020-04-19 17:19:13 -0400

    Pre-Caching assets

--- pwa-whisk-app/sw.js
+++ pwa-whisk-app/sw.js
@@ -1,14 +1,36 @@
+const staticCacheName = "site-static";
+
+// assets to cache
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+];
+
 // listen for 'install' event
-self.addEventListener("install", () => {
-  console.log("service worker has been installed");
+self.addEventListener("install", (event) => {
+  //console.log("service worker has been installed");
+  // Wait until caching is done
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
+  );
 });
 
 // listen for 'activate' event
 self.addEventListener("activate", () => {
-  console.log("service worker has been activated");
+  //console.log("service worker has been activated");
 });
 
 // listen for 'fetch' event
 self.addEventListener("fetch", (event) => {
-  console.log("fetch event", event);
+  //console.log("fetch event", event);
 });

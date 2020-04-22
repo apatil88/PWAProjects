a8e0c466f1deea35e86c74105864d2ee0cf2946a
apatil88 <amrut.patil88@hotmail.com>
2020-04-21 21:57:42 -0400

    Updating UI with data from Firebase

--- pwa-whisk-app/js/db.js
+++ pwa-whisk-app/js/db.js
@@ -5,6 +5,7 @@ db.collection("recipes").onSnapshot((snapshot) => {
     //console.log(change, change.doc.data(), change.doc.id);
     if (change.type === "added") {
       // add the document data to the webpage
+      renderRecipe(change.doc.data(), change.doc.id);
     }
     if (change.type === "removed") {
       //remove the document data from the webpage

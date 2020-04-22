95d78c37e91a26b43dda6108745c4a8444137d86
apatil88 <amrut.patil88@hotmail.com>
2020-04-21 22:20:11 -0400

    Offline data

--- pwa-whisk-app/js/db.js
+++ pwa-whisk-app/js/db.js
@@ -1,3 +1,14 @@
+// offline data
+db.enablePersistence().catch((error) => {
+  if (error.code === "Failed-precondition") {
+    // probably multiple tabs opened
+    console.log("Persistence failed");
+  } else if (error.code === "unimplemented") {
+    // lack of browser support
+    console.log("Persistence not available");
+  }
+});
+
 // real-time listener
 db.collection("recipes").onSnapshot((snapshot) => {
   //console.log(snapshot.docChanges());
